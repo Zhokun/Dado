a440c97879584f3afec86b3324fcea4f65d88f8e
Zhokun <darlanjogos@gmail.com>
2021-07-25 15:51:59 -0300

    App dice finished

--- LetsPlay.py
+++ LetsPlay.py
@@ -1,44 +1,235 @@
-from random import *
 from tkinter import *
-from tkinter import messagebox
+# from tkinter import messagebox
+from random import *
+from PIL import ImageTk, Image
 
 
 class Dice(Tk):
-    """ This is a simple dice guessing game
-            We're gonna have numbers to click and a dice to show the result
-    """
     def __init__(self):
         Tk.__init__(self)
-        # Set main features for the game window
-        self.dicelist = []
-        self.geometry('400x400+2000+500')
-        self.title('Dice Guessing Game')
-        # Load images into a dict
-        # for dicenumber in range(1, 7):
-        #     imgdice = PhotoImage(file=f'D{dicenumber}.jpg')
-        #     self.dicelist.append(imgdice)
+        # Define features of Game Window
+        self.geometry('400x400+2000+500')  # Geometry and position on screen
+        self.title('Dice game')  # Window title
+        self.iconphoto(True, PhotoImage(file='Da.png'))  # Icon / My signature
+        self.config(background='white')
+        # self.img = ImageTk.PhotoImage(Image.open('D1.png'))
+        self.dices = {}
+        # Store all images inside a dictionary
+        for dicenb in range(1, 7):
+            self.img = ImageTk.PhotoImage(Image.open(f'D{dicenb}.png'))
+            self.dices[dicenb] = self.img
 
-        # Define Frames to position elements such as dice image and buttons
-        # Dice frame positioned in the middle of screen
-        self.dice_frame = LabelFrame(self, text='Dice')
+        # Dice Frame
+        self.dice_frame = Frame(self)
         self.dice_frame.pack()
-        self.mess = Label(self.dice_frame, text='teste')
-        self.mess.pack()
-        # Frame to position all numbers from 1 to 6
-        self.choice = LabelFrame(self, text='Choice')
-        self.choice.pack()
+        # Dice image inside Dice Frame
+        self.diceimg = Label(self.dice_frame, image=self.img, border=0)
+        self.diceimg.pack()
+        # Set a tuple to store default font
+        fonttext = ('Verdana', 10)
+        fontnumbersize = ('Verdana', 25)
+        # Message befor numbers
+        self.message_frame = Frame(self)
+        self.message_frame.pack()
+        message = 'Which number do you think the dice is going to stop at?'
+        self.message_label = Label(self.message_frame, text=message, font=fonttext, background='white')
+        self.message_label.pack()
+        # Numbers to choose Frame
+        self.n_choose_frame1 = Frame(self)
+        self.n_choose_frame1.pack()
+        self.n_choose_frame2 = Frame(self)
+        self.n_choose_frame2.pack()
+        # Numbered buttons
+        self.n1 = Button(self.n_choose_frame1, text='1', width=3, font=fontnumbersize,
+                         command=self.opn1, background='white')
+        self.n1.pack(side=LEFT, anchor=N)
+        self.n2 = Button(self.n_choose_frame1, text='2', width=3, font=fontnumbersize,
+                         command=self.opn2, background='white')
+        self.n2.pack(side=LEFT, anchor=N)
+        self.n3 = Button(self.n_choose_frame1, text='3', width=3, font=fontnumbersize,
+                         command=self.opn3, background='white')
+        self.n3.pack(side=LEFT, anchor=N)
+        self.n4 = Button(self.n_choose_frame2, text='4', width=3, font=fontnumbersize,
+                         command=self.opn4, background='white')
+        self.n4.pack(side=LEFT, anchor=S)
+        self.n5 = Button(self.n_choose_frame2, text='5', width=3, font=fontnumbersize,
+                         command=self.opn5, background='white')
+        self.n5.pack(side=LEFT, anchor=S)
+        self.n6 = Button(self.n_choose_frame2, text='6', width=3, font=fontnumbersize,
+                         command=self.opn6, background='white')
+        self.n6.pack(side=LEFT, anchor=S)
 
-        # Imagem
-        img = PhotoImage(file='dicesimg/D2.jpg')
-        self.imgdice = Label(self.dice_frame, image=None)
-        self.config(image=img)
+        # Victorious?
+        self.r_frame = Frame(self)
+        self.r_frame.pack()
+        self.result = Label(self.r_frame, background='white')
+        self.result.pack()
 
-        self.button_start = Button(self.choice, text='start', )
-        self.button_start.pack(side=LEFT)
-        self.button_stop = Button(self.choice, text='stop', )
+        self.tempo = 0
+
+    # Button 1
+    def opn1(self):
+        start = True
+        randimg = randint(1, 6)
+        self.result.config(text='')
+        self.diceimg.config(image=self.dices[randimg])
+
+        if self.tempo > 0.95:
+            self.tempo = 0
+            start = False  # Stop the next image from appearing
+            # self.after_cancel(cancela)
+            if randimg == 1:  # Check if user choice equals dice, if so, the message 'You Won' will appear
+                self.result.config(text='You Won', font=('Verdana', 20), fg='green')
+            else:
+                self.result.config(text='Better luck next time!', font=('Verdana', 20), fg='red')
+
+        self.tempo += 0.05
+        print(self.tempo)
+        # A simple timer to change image e make it more dynamic
+        if start:
+            if self.tempo < 0.3:
+                self.after(100, self.opn1)
+            elif self.tempo < 0.7:
+                self.after(200, self.opn1)
+            elif self.tempo < 1:
+                self.after(300, self.opn1)
+
+    # Button 2
+    def opn2(self):
+        start = True
+        randimg = randint(1, 6)
+        self.result.config(text='')
+        self.diceimg.config(image=self.dices[randimg])
+
+        if self.tempo > 0.95:
+            self.tempo = 0
+            start = False  # Stop the next image from appearing
+            # self.after_cancel(cancela)
+            if randimg == 2:  # Check if user choice equals dice, if so, the message 'You Won' will appear
+                self.result.config(text='You Won', font=('Verdana', 20), fg='green')
+            else:
+                self.result.config(text='Better luck next time!', font=('Verdana', 20), fg='red')
+
+        self.tempo += 0.05
+        print(self.tempo)
+        # A simple timer to change image e make it more dynamic
+        if start:
+            if self.tempo < 0.3:
+                self.after(100, self.opn2)
+            elif self.tempo < 0.7:
+                self.after(200, self.opn2)
+            elif self.tempo < 1:
+                self.after(300, self.opn2)
+
+    # Button 3
+    def opn3(self):
+        start = True
+        randimg = randint(1, 6)
+        self.result.config(text='')
+        self.diceimg.config(image=self.dices[randimg])
+
+        if self.tempo > 0.95:
+            self.tempo = 0
+            start = False  # Stop the next image from appearing
+            # self.after_cancel(cancela)
+            if randimg == 3:  # Check if user choice equals dice, if so, the message 'You Won' will appear
+                self.result.config(text='You Won', font=('Verdana', 20), fg='green')
+            else:
+                self.result.config(text='Better luck next time!', font=('Verdana', 20), fg='red')
+
+        self.tempo += 0.05
+        print(self.tempo)
+        # A simple timer to change image e make it more dynamic
+        if start:
+            if self.tempo < 0.3:
+                self.after(100, self.opn3)
+            elif self.tempo < 0.7:
+                self.after(200, self.opn3)
+            elif self.tempo < 1:
+                self.after(300, self.opn3)
+
+    # Button 4
+    def opn4(self):
+        start = True
+        randimg = randint(1, 6)
+        self.result.config(text='')
+        self.diceimg.config(image=self.dices[randimg])
+
+        if self.tempo > 0.95:
+            self.tempo = 0
+            start = False  # Stop the next image from appearing
+            # self.after_cancel(cancela)
+            if randimg == 4:  # Check if user choice equals dice, if so, the message 'You Won' will appear
+                self.result.config(text='You Won', font=('Verdana', 20), fg='green')
+            else:
+                self.result.config(text='Better luck next time!', font=('Verdana', 20), fg='red')
+
+        self.tempo += 0.05
+        print(self.tempo)
+        # A simple timer to change image e make it more dynamic
+        if start:
+            if self.tempo < 0.3:
+                self.after(100, self.opn4)
+            elif self.tempo < 0.7:
+                self.after(200, self.opn4)
+            elif self.tempo < 1:
+                self.after(300, self.opn4)
+
+    # Button 5
+    def opn5(self):
+        start = True
+        randimg = randint(1, 6)
+        self.result.config(text='')
+        self.diceimg.config(image=self.dices[randimg])
+
+        if self.tempo > 0.95:
+            self.tempo = 0
+            start = False  # Stop the next image from appearing
+            # self.after_cancel(cancela)
+            if randimg == 5:  # Check if user choice equals dice, if so, the message 'You Won' will appear
+                self.result.config(text='You Won', font=('Verdana', 20), fg='green')
+            else:
+                self.result.config(text='Better luck next time!', font=('Verdana', 20), fg='red')
+
+        self.tempo += 0.05
+        print(self.tempo)
+        # A simple timer to change image e make it more dynamic
+        if start:
+            if self.tempo < 0.3:
+                self.after(100, self.opn5)
+            elif self.tempo < 0.7:
+                self.after(200, self.opn5)
+            elif self.tempo < 1:
+                self.after(300, self.opn5)
+
+# Button 6
+    def opn6(self):
+        start = True
+        randimg = randint(1, 6)
+        self.result.config(text='')
+        self.diceimg.config(image=self.dices[randimg])
+
+        if self.tempo > 0.95:
+            self.tempo = 0
+            start = False  # Stop the next image from appearing
+            # self.after_cancel(cancela)
+            if randimg == 6:  # Check if user choice equals dice, if so, the message 'You Won' will appear
+                self.result.config(text='You Won', font=('Verdana', 20), fg='green')
+            else:
+                self.result.config(text='Better luck next time!', font=('Verdana', 20), fg='red')
+
+        self.tempo += 0.05
+        print(self.tempo)
+        # A simple timer to change image e make it more dynamic
+        if start:
+            if self.tempo < 0.3:
+                self.after(100, self.opn6)
+            elif self.tempo < 0.7:
+                self.after(200, self.opn6)
+            elif self.tempo < 1:
+                self.after(300, self.opn6)
 
 
-# Start the app in case it is executed from main file
 if __name__ == '__main__':
     app = Dice()
     app.mainloop()
